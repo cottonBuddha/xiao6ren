fe55fa32ad983c2617b500424e8ee86871c660ce
cottonBuddha <jqs@cottonBuddha.local>
2018-04-14 20:54:43 +0800

    部分修改

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ if (count < 3) {
     for (var i = 0; i < argsCount; i++) {
         argsArr.push(process.argv[i + 2]);
     }
-    if (argsCount === 1 && argsArr[0] === 'now') {        
-        LiuRen.nowDateTell();
-    } else if (argsCount === 1 && argsArr[0] === '-h') {
-        console.log(`
+    if (argsCount === 1) {    
+        if (argsArr[0] === 'now') {
+            LiuRen.nowDateTell();
+        }
+        if (argsArr[0] === '-h') {
+            console.log(`
             有个小伙伴，有天神秘兮兮说能帮我预测吉凶，让我随意说三个数字，
             还说很准，她平时遇到事情也常常来一发。于是我便认识了小六壬。haha。
 
@@ -24,8 +26,11 @@ if (count < 3) {
             1）不接参数按现在的时间起卜。
             2）输入未来的年、月、日、时（公历），如 x6r 2018 4 15 2。
             3）自己随意给出三个数字，如 x6r 10 2 3。
-        `);
-        
+        `);  
+        }
+        if (argsArr[0] === '-v') {
+            console.log('v1.0.1');  
+        }
     } else if (argsCount === 3) {
         const result = LiuRen.randomThreesTell(argsArr[0], argsArr[1], argsArr[2]);  
     } else if (argsCount === 4) {
